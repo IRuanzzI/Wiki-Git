c84af9b0f6ab965ae5a865a3d8a52eaae1f42fa1
Ruan Pablo <rp824194@gmail.com>
2024-08-12 12:19:22 -0300

    inicio

---
+++ wiki/src/pages/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import git from '../assets/git.png'
+import { Container } from './styles';
+function App() {
+  return (
+    <Container>
+      <img src = {git} height={72} width={72} alt='git'/>
+    </Container>
+  );
+}
+/*#TODO CRIAR COMPONETE DE INPUT*/
+export default App;
---
+++ wiki/src/pages/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 90vh;
+
+    display: flex;
+    margin: 20px 0 0;
+    alignItems: flex-start;
+    justify-content: center;
+
+
+
+`
